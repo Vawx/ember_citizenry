994fd7f2e5f0f3c15076c9fedec308cf71785593
kyle <altck8@gmail.com>
2016-03-01 16:11:09 -0800

    add url links to bills, forwarding user to congress.gov

--- app/routes/house-result.js
+++ app/routes/house-result.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
+    if(params.zip == undefined) {
+      params.zip = 10000;
+    }
     var url = "http://congress.api.sunlightfoundation.com/committees?chamber=house&apikey=0613f3c5dde44d699a1a8c7adb2e6ed7";
     return Ember.$.getJSON( url ).then(function(response) {
       var house = [];
